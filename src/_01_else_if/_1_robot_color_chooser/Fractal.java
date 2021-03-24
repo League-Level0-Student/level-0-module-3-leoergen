d3401e1d1992aac4d93e44261a8ca0b22d9dc74e
leoer <leoer@DESKTOP-HBVJVS0>
2021-03-23 18:28:29 -0700

    un in iv fi er

---
+++ src/_01_else_if/_1_robot_color_chooser/Fractal.java
@@ -0,0 +1,31 @@
+package _01_else_if._1_robot_color_chooser;
+
+public class Fractal {
+	public void MBrot()
+	{
+	    float epsilon = 0.0001; // The step size across the X and Y axis
+	    float x;
+	    float y;
+	    int maxIterations = 10; // increasing this will give you a more detailed fractal
+	    int maxColors = 256; // Change as appropriate for your display.
+
+	    Complex Z;
+	    Complex C;
+	    int iterations;
+	    for(x=-2; x<=2; x+= epsilon)
+	    {
+	        for(y=-2; y<=2; y+= epsilon)
+	        {
+	            iterations = 0;
+	            C = new Complex(x, y);
+	            Z = new Complex(0,0);
+	            while(Complex.Abs(Z) < 2 && iterations < maxIterations)
+	            {
+	                Z = Z*Z + C;
+	                iterations++;
+	            }
+	            Screen.Plot(x,y, iterations % maxColors); //depending on the number of iterations, color a pixel.
+	        }
+	    }
+	}
+}
--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -1,23 +1,35 @@
 
 package _01_else_if._1_robot_color_chooser;
+import org.jointheleague.graphical.robot.Robot;
 
 import javax.swing.JOptionPane;
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		JOptionPane.showInputDialog(null, "what color would you like? Leave empty for random.");
 		
 		//1. Create a new Robot
-	    //2. Make the robot draw a shape (this will take more than one line of code)		
+		Robot rob = new Robot("batman" );
+		Robot rud = new Robot();
+		rob.setSpeed(100);
+		rud.setSpeed(100);
+	    //2. Make the robot draw a shape (this will take more than one line of code)'
+		rob.penDown();
+		rud.penDown();
+		rob.setPenWidth(100);
+		for (int i = 0; i < 1000; i++) {
+			rob.move(100); 
+			rob.turn(100);
+			rud.turn(-200);
+			rud.move(-200); 
+		}
 		//3. Set the pen width to 10
 		//4. Ask the user what color pen they would like the robot to draw with
+		
 		//5. Use an if/else statement to set the pen color that the user requested
         //6. If the user doesn't enter anything, choose a random color
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-
-		
-
 
 
 	}
