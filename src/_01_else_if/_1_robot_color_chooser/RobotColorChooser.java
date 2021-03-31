9dc6be52e813e3731817a940c6b0c6f27746d0e1
School <School@DESKTOP-L9UDOFJ>
2021-03-30 18:34:41 -0700

    vividd trianglee... NJkufjkuGKUuZDk

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -9,13 +9,17 @@ import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+			
 		
 		//1. Create a new Robot
-		Robot rob = new Robot();
+		Robot rob = new Robot("mini");
+		
+		for (int i = 0; i < 50; i++) {
 	    //2. Make the robot draw a shape (this will take more than one line of code)	
-		rob.move(20);
 		//3. Set the pen width to 10
-		rob.setPenWidth(10);
+		rob.setPenWidth(100);
+		
+		rob.penDown();
 		//4. Ask the user what color pen they would like the robot to draw with
 		String clr = JOptionPane.showInputDialog(null, "what colo(u)r would you like?");
 		
@@ -23,10 +27,41 @@ public class RobotColorChooser {
 		if (clr.equals("red")) {
 			rob.setPenColor(Color.red);
 		}
+		
+		else if (clr.equals("yellow")) {
+			rob.setPenColor(Color.yellow);
+		}
+		
+		else if (clr.equals("orange")) {
+			rob.setPenColor(Color.orange);
+		}
+		
+		else if (clr.equals("green")) {
+			rob.setPenColor(Color.green);
+		}
+		
+		else if (clr.equals("blue")) {
+			rob.setPenColor(Color.blue);
+		}
+		
+		else if (clr.equals("pink")) {
+			rob.setPenColor(Color.pink);
+		}
+		
+		else if (clr.equals("black")) {
+			rob.setPenColor(Color.black);
+		}
         //6. If the user doesn't enter anything, choose a random color
-		
+		else {
+		rob.setRandomPenColor();
+		}
+		rob.setSpeed(100);
+		rob.move(50);
+		rob.turn(-111);
+		rob.move(50);
+		rob.turn(-11);
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+		}
 
 		
 
