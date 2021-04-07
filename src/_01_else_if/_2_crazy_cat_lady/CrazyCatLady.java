29f7ddaba99287efb1c19b0163b6acc53113aa99
School <School@10.0.0.23>
2021-04-06 18:23:10 -0700

    reaching rock bottom

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,15 +3,31 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String cat = JOptionPane.showInputDialog("how many cats do you have");
 		// 2. Convert their answer into an int
+		int feline= Integer.parseInt(cat);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if (feline>=3){
+			JOptionPane.showMessageDialog(null, "you are the crazy cat lady of your neighorhood");
+		}
+		
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+		else if (feline<3&&feline>0){
+			playVideo("https://www.youtube.com/watch?v=nRzsgCp60YU");
+		}
+		
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+		
+		else{
+			playVideo("https://www.youtube.com/watch?v=wbr5vkIo4K8");
+		}
 		
 		
 		
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -3,17 +3,20 @@ package _01_else_if._3_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(100)+1;
 		
 		// 2. Print out the random variable above
 		
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
+JOptionPane.showInputDialog("choose a number between 1 and 100 you will get ten chances to guess this number");
 
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
 			
