9497ceff25a69c4f941739c872e3c0bc1e32ec11
School <School@10.0.0.23>
2021-04-27 18:31:14 -0700

    stowyyyyyy1

---
+++ src/_01_else_if/_5_own_adventure/StoryTime.java
@@ -0,0 +1,15 @@
+package _01_else_if._5_own_adventure;
+
+import javax.swing.JOptionPane;
+
+public class StoryTime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+JOptionPane.showMessageDialog(null, "Once lived a chaotic, not very bright kid.");
+JOptionPane.showMessageDialog(null, "He is planning on going out to town as he is 15 he can now become what he has always dreamed of, a doctor");
+JOptionPane.showInputDialog("Doctors in this land were extremely important and can even be compared to heros with how they go out, and risk their lives for humanity, but for all that to begin you need a setting (type: dessert, forest, or city");
+
+	}
+
+}
