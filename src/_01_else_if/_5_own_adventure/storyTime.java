442b86d6ae79dbb9b99087b3974f97fe20f03ef5
leoer <leoer@DESKTOP-HBVJVS0>
2021-05-04 18:31:18 -0700

    storry

--- src/_01_else_if/_5_own_adventure/storyTime.java
+++ src/_01_else_if/_5_own_adventure/storyTime.java
@@ -1,10 +1,17 @@
 package _01_else_if._5_own_adventure;
 
+import javax.swing.JOptionPane;
+
 public class storyTime {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+String nam = JOptionPane.showInputDialog(" Our story begins in Garan where a meschivious boy is causing havoc again. he is getting ready to become a doctor. Doctors in this land are comarible to superheros minus the super and plus the medicine, but more important than that is his name: ");
+JOptionPane.showMessageDialog(null, nam + " is packing his stuff as of right now andd getting ready to go out and explore the world for medicines, vaccines, and more.");
+String scl = JOptionPane.showInputDialog("Of course " + nam + " knew exactly which medical school to go to and its name is: ");
+JOptionPane.showMessageDialog(null, scl + " was one of the most popular schools with and estimated 10% chance of somone passing the initial test to be let in and " + nam + " got a letter saying he passed and he will soon get his official " + scl + " tatoo on both his knees (so they can be consealed)soon");
+JOptionPane.showMessageDialog(null, nam + " ran as fast as he could to the school (also required that that students dont use any form of transportation on their walks there) to " + scl);
+JOptionPane.showMessageDialog(null, nam + " bursted through the front gates revealing a huge frontyard and upon entering the the doors to the main hall there is a huge bulletin board with requests from patients and nurses for various items like moon flowers, gorial seeds, and even hurglegumph berries! ");
 	}
 
 }
