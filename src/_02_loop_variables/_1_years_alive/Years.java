3f695b6e5f829df5705d6fb7c4a38dc0779ee072
School <School@10.0.0.23>
2021-05-11 18:27:37 -0700

    mr zee requires your objects of sparkling

---
+++ src/_02_loop_variables/_1_years_alive/Years.java
@@ -0,0 +1,15 @@
+package _02_loop_variables._1_years_alive;
+
+import java.awt.print.Printable;
+
+public class Years {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ for (int i = 2010; i <= 2021; i++) {
+	System.out.println(i);
+}
+ System.out.println();
+	}
+
+}
--- src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
+++ src/_02_loop_variables/_2_shiny_objects/ShinyObjects.java
@@ -3,8 +3,11 @@ package _02_loop_variables._2_shiny_objects;
 
 
 import java.io.File;
+import java.text.ParseException;
+
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 
@@ -24,13 +27,15 @@ public class ShinyObjects {
 	void manyShinyObjects() {
 		
 		// 1. Call the method below to play Mister Zee
-		
+		File sound;
 		
 		// 2. Ask the user how many shiny objects they want
-		
-		
+		String s = JOptionPane.showInputDialog("how many shiny things do you want?");
+		int f = Integer.parseInt(s);
 		// 3. Play the sound that many times
-
+for (int i = 0; i <= f; i++) {
+	playMisterZee();
+}
 
 	}
 
