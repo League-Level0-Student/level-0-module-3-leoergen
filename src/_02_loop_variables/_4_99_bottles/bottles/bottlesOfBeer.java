7c60f185ff634f8b7c930327e4f468173ab158dd
School <School@10.0.0.23>
2021-05-18 18:29:48 -0700

    oh look its the third star! so cool

---
+++ src/_02_loop_variables/_4_99_bottles/bottles/bottlesOfBeer.java
@@ -0,0 +1,12 @@
+package _02_loop_variables._4_99_bottles.bottles;
+
+public class bottlesOfBeer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 0; i < args.length; i++) {
+	System.out.println(99 bottles of beer on the wall);
+}
+	}
+
+}
