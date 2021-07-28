cb941102014af42ac6ebab23caa82996d04d6a3e
School <School@DESKTOP-L9UDOFJ>
2021-07-27 18:25:08 -0700

    fizzy bussy

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -26,14 +26,19 @@ import javax.swing.JOptionPane;
 public class FizzBuzz {
 	public static void main(String[] args) {
 		for (int i = 1; i < 21; i++) {
-			System.out.println(i);
-		if(i%3==0) {
-			System.out.println("Fizz");
-		}
+			 if(i % 3 == 0 && i % 5 == 0) {
+					System.out.println("FizzBuzz");
+				}
+		
 		else if(i%5==0) {
-			System.out.println("buzz");
+			System.out.println("Buzz");
+		}
+		else if(i%3==0) {
+				System.out.println("Fizz");
+		}
+		else{
+			System.out.println(i);
 		}
 		}
-		}
-}
+	}
 }
